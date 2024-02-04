ef4b464b5d201cd8403264ef8cd1facaed9ed41e
Ahn Geunsu <155143646+maybeags@users.noreply.github.com>
2024-02-05 02:15:56 +0900

    learning JS grammatical items

--- variable.js
+++ variable.js
@@ -3,7 +3,7 @@
 // use this for Valina Javascript.
 'use strict';
 
-// 2. Variable
+// 2. Variable, rw(read/write)
 // let (added in ES 6)
 let globalName = 'global name';
 {
@@ -25,13 +25,20 @@ console.log(globalName);
 }
 console.log(age);
 
-// 3. Constants
+// 3. Constant, r(read only)
+// use const whenever possible
+// only use let if variable needs to change.
+
 // favor immutable data type always for a few reasons:
 // - security
 // - thread safety
 // - reduce human mistakes
 const daysInWeek = 7;
 const maxNumber = 5;
+
+// Note!
+// Immutable data types: primitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 
 // 4. Variable types
 // primitive, single item : number, string, boolean, null, undefined, symbol
